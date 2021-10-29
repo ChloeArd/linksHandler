fa2d78513ca490909f07d8ee57ed8bd96e633409
ChloeArd <chloe.ardoise@gmail.com>
2021-10-29 11:07:42 +0200

    page contact

--- public/assets/php/contact.php
+++ public/assets/php/contact.php
@@ -5,12 +5,14 @@ if (isset($_POST["email"], $_POST["subject"], $_POST['message'])) {
     $email = htmlentities(trim($_POST['email']));
     $subject = htmlentities(trim($_POST['subject']));
     $message = htmlentities(trim($_POST['message']));
+    $message = wordwrap($message, 70, "\r\n");
+    $headers = array(
+        'Reply-To' => 'chloe.ardoise@gmail.com',
+        'Cc' => 'chloe@chloeard.fr',
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        mail($email, $subject, $message);
-        echo $email . "<br>" . $subject . "<br>" . $message;
-    }
-    else {
-        header("Location: ../../index.php?controller=home&page=contact&error=0");
-    }
+    mail("chloe@chloeard.fr", $subject, $message, $headers, "-f . $email");
+
+    header("Location: ../../index.php?controller=home&page=contact&success=0");
 }
