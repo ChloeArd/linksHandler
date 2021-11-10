a68f3152f0ee9273ffb6ca1fc1edeb07768f6f0a
ChloeArd <chloe.ardoise@gmail.com>
2021-11-10 10:11:48 +0100

    modification

--- public/assets/php/contact.php
+++ public/assets/php/contact.php
@@ -12,8 +12,14 @@ if (isset($_POST["email"], $_POST["subject"], $_POST['message'])) {
         'Cc' => 'chloe@chloeard.fr',
         'X-Mailer' => 'PHP/' . phpversion()
     );
-
-    mail($to, $subject, $message, $headers, "-f " . $email);
-
-    header("Location: ../../index.php?controller=home&page=contact&success=0&message=Envoyer%20avec%20succés");
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        mail($to, $subject, $message, $headers, "-f " . $email);
+        header("Location: ../../index.php?controller=home&page=contact&success=0&message=Envoyer%20avec%20succés");
+    }
+    else {
+        header("Location: ../../index.php?controller=home&page=contact&error=0&message=L'email%20n'est%20pas%20valide.");
+    }
+}
+else {
+    header("Location: ../../index.php?controller=home&page=contact&error=1&message=Tous%20les%20champs%20ne%20sont%20pas%20complétés.");
 }
